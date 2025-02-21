4d771bdcd29f4323f1e3452587a10db1fbd91c0b
AsilanAsKa <asilanserikbay25@gmail.com>
2025-02-21 14:31:10 +0800

    hello

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,20 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape square = new Square(4);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer[] renderers = { new Renderer(circle), new Renderer(rectangle), new Renderer(square) };
 
-        renderer.draw();
+        for (Renderer renderer : renderers) {
+            renderer.draw();
+        }
     }
 }
